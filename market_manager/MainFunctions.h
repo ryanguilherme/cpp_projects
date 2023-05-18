24faf20458b548d0e0e0fa545f43c33cc1178684
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-05-18 11:55:16 -0300

    added some sytem(clear) to improve user experience

--- market_manager/MainFunctions.h
+++ market_manager/MainFunctions.h
@@ -9,6 +9,96 @@
 #ifndef MARKET_MANAGER_MAINFUNCTIONS_H
 #define MARKET_MANAGER_MAINFUNCTIONS_H
 
+void accountMenu( bool *logged, std::vector<Account> *accountList ) {
+    int option;
+    std::cout << "Choose an option: ";
+    std::cin >> option;
+
+    if ( option == 1 ) {
+        bool registered = false;
+        while (!registered) {
+            std::string login;
+            std::cout << "Login: ";
+            std::cin >> login;
+            bool loginExists = false;
+            for (auto account : *accountList) {
+                if ( account.getLogin() == login ) {
+                    std::cout << "Login already exists" << std::endl;
+                    loginExists = true;
+                    break;
+                }
+            }
+            if ( loginExists ) {
+                loginExists = false;
+                continue;
+            }
+            std::string password;
+            std::cout << "Password: ";
+            std::cin >> password;
+
+            // add account to a vector of accounts
+            system("clear");
+            accountList->emplace_back(login, password);
+            registered = true;
+            std::cout << "Account registered successfully" << std::endl;
+
+        }
+    } else if ( option == 2 ) {
+        std::string login;
+        std::cout << "Login: ";
+        std::cin >> login;
+        for ( auto account : *accountList ) {
+            if ( account.getLogin() == login ) {
+                std::string password;
+                std::cout << "Password: ";
+                std::cin >> password;
+                if ( account.getPassword() == password ) {
+                    system("clear");
+                    std::cout << "Login successfully" << std::endl;
+                    *logged = true;
+                    break;
+                } else {
+                    system("clear");
+                    std::cout << "Incorrect password" << std::endl;
+                    break;
+                }
+            }
+            system("clear");
+            std::cout << "Invalid login" << std::endl;
+            break;
+        }
+    } else if ( option == 0 ) {
+        std::cout << std::endl << "Thank you for using Market Manager" << std::endl << "Exiting..." << std::endl;
+        exit(0);
+    } else {
+        std::cout << std::endl << "error: Invalid option!" << std::endl;
+    }
+}
+
+void addItem( std::vector<Item> *itemList ) {
+    std::string itemName;
+    std::cout << "Item Name: ";
+    std::cin >> itemName;
+    bool validItem = true;
+    for ( auto item : *itemList) {
+        if ( item.getName() == itemName ) {
+            validItem = false;
+            std::cout << "Item already exists" << std::endl;
+            break;
+        }
+    }
+    if ( validItem ) {
+        double itemPrice, itemWeight;
+        std::cout << "Price: ";
+        std::cin >> itemPrice;
+        std::cout << "Weight: ";
+        std::cin >> itemWeight;
+        // add new item to items vector
+        itemList->emplace_back( itemName, itemPrice, itemWeight );
+        std::cout << "Item successfully added" << std::endl;
+    }
+}
+
 void removeItem( std::vector<Item> *itemList ) {
     if ( itemList->empty() ) {
         std::cout << "No item to remove!" << std::endl;
--- market_manager/main.cpp
+++ market_manager/main.cpp
@@ -17,6 +17,7 @@ void loginMenu() {
             "+--------------------+\n"
             "| 1 - Create Account |\n"
             "| 2 - LOGIN          |\n"
+            "| 0 - Exit           |\n"
             "+--------------------+\n"
             "";
 }
@@ -39,7 +40,7 @@ void menu() {
 
 int main() {
 
-    bool running = true;
+    // data
     bool logged = false;
     vector<Item> itemList;
     vector<Account> accountList;
@@ -48,106 +49,33 @@ int main() {
     loginScreen:
     while ( !logged ) {
         loginMenu();
-        int option;
-        cout << "choose an option: ";
-        cin >> option;
-
-        if ( option == 1 ) {
-            bool registered = false;
-            while (!registered) {
-                string login;
-                cout << "Login: ";
-                cin >> login;
-                bool loginExists = false;
-                for (auto account : accountList) {
-                    if ( account.getLogin() == login ) {
-                        cout << "Login already exists" << endl;
-                        loginExists = true;
-                        break;
-                    }
-                }
-                if ( loginExists ) {
-                    loginExists = false;
-                    continue;
-                }
-                string password;
-                cout << "Password: ";
-                cin >> password;
-
-                // add account to a vector of accounts
-                accountList.emplace_back(login, password);
-                registered = true;
-
-            }
-        } else if ( option == 2 ) {
-            string login;
-            cout << "Login: ";
-            cin >> login;
-            for ( auto account : accountList ) {
-                if ( account.getLogin() == login ) {
-                    string password;
-                    cout << "Password: ";
-                    cin >> password;
-                    if ( account.getPassword() == password ) {
-                        cout << "Login successfully" << endl;
-                        logged = true;
-                        break;
-                    } else {
-                        cout << "Incorrect password" << endl;
-                        break;
-                    }
-                }
-                cout << "Invalid login" << endl;
-                break;
-            }
-        }
+        accountMenu( &logged, &accountList );
     }
+
     // principal menu
     while ( logged ) {
         menu();
         int option;
-        cout << "choose an option: ";
+        cout << "| Choose an option: ";
         cin >> option;
 
         // add item
-        if ( option == 1 ) {
-            string itemName;
-            cout << "Item Name: ";
-            cin >> itemName;
-            bool validItem = true;
-            for ( auto item : itemList) {
-                if ( item.getName() == itemName ) {
-                    validItem = false;
-                    cout << "Item already exists" << endl;
-                    break;
-                }
-            }
-            if ( validItem ) {
-                double itemPrice, itemWeight;
-                cout << "Price: ";
-                cin >> itemPrice;
-                cout << "Weight: ";
-                cin >> itemWeight;
-                // add new item to items vector
-                itemList.emplace_back( itemName, itemPrice, itemWeight );
-                cout << "Item successfully added" << endl;
-            }
-        }
+        if ( option == 1 ) { addItem( &itemList );      }
 
-        if ( option == 2 ) {
+        // remove item
+        if ( option == 2 ) { removeItem( &itemList );   }
 
-            removeItem( &itemList );
+        // list items
+        if ( option == 3 ) { listItems( itemList );     }
 
-        }
-
-        if ( option == 3) {
-
-            listItems( itemList );
-
-        }
 
         if ( option == 4 ) {
             
+        }
+
+        if ( option == 0 ) {
+            logged = false;
+            goto loginScreen;
         }
 
 
