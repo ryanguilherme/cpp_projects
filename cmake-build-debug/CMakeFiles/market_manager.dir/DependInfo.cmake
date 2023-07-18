3df254084759ab2a87934d744aa3c51f03474432
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-07-18 16:54:20 -0300

    cmake

---
+++ cmake-build-debug/CMakeFiles/market_manager.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ryan/Documents/code/market_manager/src/Account.cpp" "CMakeFiles/market_manager.dir/src/Account.cpp.o" "gcc" "CMakeFiles/market_manager.dir/src/Account.cpp.o.d"
+  "/home/ryan/Documents/code/market_manager/src/Item.cpp" "CMakeFiles/market_manager.dir/src/Item.cpp.o" "gcc" "CMakeFiles/market_manager.dir/src/Item.cpp.o.d"
+  "/home/ryan/Documents/code/market_manager/src/MainFunctions.cpp" "CMakeFiles/market_manager.dir/src/MainFunctions.cpp.o" "gcc" "CMakeFiles/market_manager.dir/src/MainFunctions.cpp.o.d"
+  "/home/ryan/Documents/code/market_manager/src/Stock.cpp" "CMakeFiles/market_manager.dir/src/Stock.cpp.o" "gcc" "CMakeFiles/market_manager.dir/src/Stock.cpp.o.d"
+  "/home/ryan/Documents/code/market_manager/src/main.cpp" "CMakeFiles/market_manager.dir/src/main.cpp.o" "gcc" "CMakeFiles/market_manager.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/market_manager.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/market_manager.dir/src/Account.cpp.o"
+  "CMakeFiles/market_manager.dir/src/Account.cpp.o.d"
+  "CMakeFiles/market_manager.dir/src/Item.cpp.o"
+  "CMakeFiles/market_manager.dir/src/Item.cpp.o.d"
+  "CMakeFiles/market_manager.dir/src/MainFunctions.cpp.o"
+  "CMakeFiles/market_manager.dir/src/MainFunctions.cpp.o.d"
+  "CMakeFiles/market_manager.dir/src/Stock.cpp.o"
+  "CMakeFiles/market_manager.dir/src/Stock.cpp.o.d"
+  "CMakeFiles/market_manager.dir/src/main.cpp.o"
+  "CMakeFiles/market_manager.dir/src/main.cpp.o.d"
+  "market_manager"
+  "market_manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/market_manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
